9dd70f58547f7d5e7a007368a36d94d1fec02058
oscarnanito <oherna15@estudiante.ibero.edu.co>
2026-05-29 21:42:22 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const appointmentsRoutes = require("./src/routes/cars.routes");
 const swaggerDocs = require("./src/swagger");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors());
