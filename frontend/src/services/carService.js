9c5b62e531ed86eb9be0eeb062e1b21ffd7c3587
oscarnanito <oherna15@estudiante.ibero.edu.co>
2026-05-29 21:54:19 -0500

    Update carService.js

--- frontend/src/services/carService.js
+++ frontend/src/services/carService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/appointments";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api/appointments";
 
 // Obtener todas las citas
 export const getAllAppointments = async () => {
